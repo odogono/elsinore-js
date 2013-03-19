bc93dbbeee207d4ef0dec83c6747b07b59e7df85
Alex Veenendaal <alex@opendoorgonorth.com>
2013-03-19 10:04:04 +0000

    able to query for scheme properties and property names

--- test/test.schema.js
+++ test/test.schema.js
@@ -1,12 +1,12 @@
 require('../index');
-var start = Date.now();
+// var start = Date.now();
 // var Entity = require('../lib/entity');
 var assert = require('assert');
 var schema = require('../lib/schema');
 
 // var schemaEnv = schema.env; //JSV.createEnvironment();
 
-log.debug('loaded ' + (Date.now()-start) );
+// log.debug('loaded ' + (Date.now()-start) );
 
 describe('Schema', function(){
     beforeEach(function(){
@@ -19,10 +19,10 @@ describe('Schema', function(){
         // initialise with a default namespace
         schema.initialise();
 
-        schema.register({
-            id:"http://entity.odogono.com",
-            properties:{}
-        });
+        // schema.register({
+        //     id:"http://entity.odogono.com",
+        //     properties:{}
+        // });
 
         schema.register({
             id:"http://entity.odogono.com/entity",
@@ -33,5 +33,53 @@ describe('Schema', function(){
         });
 
         assert( schema.validate( {id:"test_id"}, "http://entity.odogono.com/entity" ) );
+    });
+
+    it('should return a list of defined fields', function(){
+        
+        schema.register({
+            id:"http://entity.odogono.com/entity",
+            properties: {
+                id: {type:"string", required:true },
+                name: {type:"string", required:true },
+                status: {type:"integer", required:true },
+                type: {type:"integer", required:true },
+                created_at: {type:"string", required:true },
+                updated_at: {type:"string", required:true }
+            },
+            additionalProperties:true
+        });
+
+        assert.deepEqual(
+            schema.propertyNames("http://entity.odogono.com/entity"),
+            ["id", "name", "status", "type", "created_at", "updated_at"]
+        );
+        
+    });
+
+    it.only('should return a list of defined fields from a child', function(){
+        schema.register({
+            id:"http://entity.odogono.com/entity",
+            properties: {
+                id: {type:"string", required:true },
+                status: {type:"integer", required:true },
+            },
+            additionalProperties:true
+        });
+        schema.register({
+            id:"http://entity.odogono.com/company",
+            extends:{"$ref":"http://entity.odogono.com/entity"},
+            properties:{
+                company_id: {type:"integer", required:true }
+            }
+        });
+
+        assert( !schema.validate( {id:"test_id", status:3}, "http://entity.odogono.com/company" ) );
+        // console.log( schema.errors );
+        assert.deepEqual(
+            schema.propertyNames("http://entity.odogono.com/company"),
+            ["id", "status", "company_id"]
+        );
+        // console.log( schema.properties("http://entity.odogono.com/company") );
     });
 });
