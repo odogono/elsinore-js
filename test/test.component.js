a7ead4bb73a78a5657cec4bec7e380d75c7b3757
Alex Veenendaal <alex@odogono.com>
2013-05-23 21:25:32 +0100

    Further renaming of entity to model.
    first set of tests for components.

---
+++ test/test.component.js
@@ -0,0 +1,46 @@
+require('./common');
+var tv4 = require('tv4').tv4;
+var odgn = require('../index')();
+
+describe('Registry', function(){
+    beforeEach( function(){
+        this.registry = odgn.entity.ComponentRegistry.create();
+    });
+
+    it('should register a new component', function(){
+        var schema = {
+            "id":"/component/example",
+        };
+
+        // register a component definition
+        var componentDef = this.registry.register(schema);
+
+        // Def can be retrieved by schema id. schema is attached to def. 
+        assert.equal( this.registry.getComponentDef('/component/example').schema, componentDef.schema );
+
+        // create a new component instance with default properties
+        var component = this.registry.create('/component/example');
+
+        assert( component instanceof this.registry.getComponentDef('/component/example').Component );
+
+        // print_ins( this.registry.getComponentDef('/component/example'), false, 2 );
+        // print_ins( component, false, 2 );
+        
+        // should have a component id
+        assert.equal( component.constructor.componentDef.defId, componentDef.defId );
+    });
+
+    it('should retrieve all components of a type', function(){
+        var defA = this.registry.register( {"id":"/component/example_a"} );
+        var defB = this.registry.register( {"id":"/component/example_b"} );
+
+        for( var i=0;i<10;i++ ){
+            defA.create();
+            defB.create();
+        }
+
+        var components = this.registry.select(defA);
+
+        assert.equal( components.length, 10 );
+    });
+});
