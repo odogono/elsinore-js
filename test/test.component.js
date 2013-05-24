811aab23444ae61a71bb0c5c474b5ae04ad67733
Alex Veenendaal <alex@opendoorgonorth.com>
2013-05-24 14:12:23 +0100

    converted most component registry calls to callbacks.

--- test/test.component.js
+++ test/test.component.js
@@ -2,60 +2,85 @@ require('./common');
 var tv4 = require('tv4').tv4;
 var odgn = require('../index')();
 
-describe('Registry', function(){
+describe('Component', function(){
     beforeEach( function(){
-        this.registry = odgn.entity.ComponentRegistry.create();
+        this.cRegistry = odgn.entity.ComponentRegistry.create();
     });
 
-    it('should register a new component', function(){
-        var schema = {
-            "id":"/component/example",
-        };
+    it.only('should register a new component', function(done){
+        var self = this;
+        var schema = { "id":"/component/example" }
 
         // register a component definition
-        var componentDef = this.registry.register(schema);
+        // the act of registering component means:
+        // - it can be instantiated
+        // - it can be looked up by its schema id
+        // - it will receive a global id that can be used to reference it
+        // - it may be persisted in the attached datastore
+        this.cRegistry.register(schema, function(err, componentDef){
+            // Def can be retrieved by schema id. schema is attached to def. 
+            assert.equal( self.cRegistry.getComponentDef('/component/example').schema, componentDef.schema );
 
-        // Def can be retrieved by schema id. schema is attached to def. 
-        assert.equal( this.registry.getComponentDef('/component/example').schema, componentDef.schema );
+            // create a new component instance with default properties
+            self.cRegistry.create('/component/example', function(err,component){
+                assert( component instanceof self.cRegistry.getComponentDef('/component/example').Component );
 
-        // create a new component instance with default properties
-        var component = this.registry.create('/component/example');
-
-        assert( component instanceof this.registry.getComponentDef('/component/example').Component );
-
-        // print_ins( this.registry.getComponentDef('/component/example'), false, 2 );
-        // print_ins( component, false, 2 );
-        
-        // should have a component id
-        assert.equal( component.constructor.componentDef.defId, componentDef.defId );
+                // should have a component id
+                assert.equal( component.constructor.componentDef.defId, componentDef.defId );
+                done();
+            });
+        });
     });
 
-    it('should create a component with data', function(){
-        this.registry.register({
+    it('should create a component with data', function(done){
+        var self = this;
+        this.cRegistry.register({
             "id":"/component/data",
             "type":"object",
             "properties":{
                 "name":{ "type":"string" },
                 "count":{ "type":"integer" }
             }
+        }, function(err,cDef){
+            self.cRegistry.create("/component/data", {"name":"diamond", "count":23}, function(err,com){
+                assert.equal( com.get("name"), "diamond" );
+                assert.equal( com.get("count"), 23 );
+                done();
+            });
         });
-
-        var com = this.registry.create("/component/data", {"name":"diamond", "count":23} );
-        assert.equal( com.get("name"), "diamond" );
-        assert.equal( com.get("count"), 23 );
     });
 
-    it('should retrieve all components of a type', function(){
-        var defA = this.registry.register( {"id":"/component/example_a"} );
-        var defB = this.registry.register( {"id":"/component/example_b"} );
+    it('should retrieve all components of a type', function(done){
+        var self = this;
 
-        for( var i=0;i<10;i++ ){
-            defA.create();
-            defB.create();
-        }
-
-        var components = this.registry.select(defA);
-
-        assert.equal( components.length, 10 );
+        async.waterfall([
+            function(cb){
+                self.cRegistry.register( [{"id":"/component/example_a"}, {"id":"/component/example_b"}], cb );
+            },
+            // create 10 example_a components
+            function(defs, cb){
+                var def = self.cRegistry.getComponentDef("/component/example_a");
+                async.times(10, function(n,next){
+                    def.create(function(err,com){
+                        next(err,com);
+                    });
+                }, cb);
+            },
+            // create 10 example_b components
+            function( comsA, cb ){
+                var def = self.cRegistry.getComponentDef("/component/example_b");
+                async.times(10, function(n,next){
+                    def.create(function(err,com){
+                        next(err,com);
+                    });
+                }, cb );
+            }
+        // select all component_a components
+        ], function(err){
+            self.cRegistry.select("/component/example_a", function(err, components){
+                assert.equal( components.length, 10 );
+                done();
+            });
+        });
     });
 });
