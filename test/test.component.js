4bd00050ea484b720217274b1332ba43f06aa00a
Alex Veenendaal <alex@odogono.com>
2013-06-02 17:16:27 +0100

    hooks for component middleware. don't like the middleware term...

--- test/test.component.js
+++ test/test.component.js
@@ -16,12 +16,12 @@ describe('Component', function(){
         // - it can be looked up by its schema id
         // - it will receive a global id that can be used to reference it
         // - it may be persisted in the attached datastore
-        this.cRegistry.register(schema, function(err, componentDef){
+        this.cRegistry.registerComponent(schema, function(err, componentDef){
             // Def can be retrieved by schema id. schema is attached to def. 
             assert.equal( self.cRegistry.getComponentDef('/component/example').schema, componentDef.schema );
 
             // create a new component instance with default properties
-            self.cRegistry.create('/component/example', function(err,component){
+            self.cRegistry.createComponent('/component/example', function(err,component){
                 assert( component instanceof self.cRegistry.getComponentDef('/component/example').Component );
 
                 // should have a component id
@@ -33,7 +33,7 @@ describe('Component', function(){
 
     it('should create a component with data', function(done){
         var self = this;
-        this.cRegistry.register({
+        this.cRegistry.registerComponent({
             "id":"/component/data",
             "type":"object",
             "properties":{
@@ -41,7 +41,7 @@ describe('Component', function(){
                 "count":{ "type":"integer" }
             }
         }, function(err,cDef){
-            self.cRegistry.create("/component/data", {"name":"diamond", "count":23}, function(err,com){
+            self.cRegistry.createComponent("/component/data", {"name":"diamond", "count":23}, function(err,com){
                 assert.equal( com.get("name"), "diamond" );
                 assert.equal( com.get("count"), 23 );
                 done();
@@ -55,13 +55,13 @@ describe('Component', function(){
         async.waterfall([
             // register two components
             function(cb){
-                self.cRegistry.register( [{"id":"/component/example_a"}, {"id":"/component/example_b"}], cb );
+                self.cRegistry.registerComponent( [{"id":"/component/example_a"}, {"id":"/component/example_b"}], cb );
             },
             // create 10 example_a components
             function(defs, cb){
                 var def = self.cRegistry.getComponentDef("/component/example_a");
                 async.times(10, function(n,next){
-                    def.create(function(err,com){
+                    def.createComponent(function(err,com){
                         next(err,com);
                     });
                 }, cb);
@@ -70,7 +70,7 @@ describe('Component', function(){
             function( comsA, cb ){
                 var def = self.cRegistry.getComponentDef("/component/example_b");
                 async.times(10, function(n,next){
-                    def.create(function(err,com){
+                    def.createComponent(function(err,com){
                         next(err,com);
                     });
                 }, cb );
