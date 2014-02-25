9dce95f374be87517da7044e1270eceb04d8390d
Alex Veenendaal <alex@opendoorgonorth.com>
2014-02-25 11:15:15 +0000

    componentDef creating with default properties

---
+++ test/test.component_def.js
@@ -0,0 +1,60 @@
+require('./common');
+
+var ComponentDef = Elsinore.ComponentDef;
+var SchemaRegistry = Elsinore.SchemaRegistry;
+
+describe('ComponentDef', function(){
+
+    describe('naming', function(){
+        it('should create a name from a schema id', function(){
+            var schema = {
+                id: '/component/example'
+            };
+            ComponentDef.nameFromSchema( schema ).should.equal( 'ExampleComDef' );
+        });
+        it('should create a name from a schema id', function(){
+            var schema = {
+                id: '/component/example',
+                name: 'real_name'
+            };
+            ComponentDef.nameFromSchema( schema ).should.equal( 'RealNameComDef' );
+        });
+    });
+
+
+    describe('default properties', function(){
+        it('should return default properties', function(){
+            var schema = {
+                id: '/component/example',
+                properties:{
+                    name:{ type:'string', 'default':'pepper' }
+                }
+            };
+
+            // result.should.equal( { name:'pepper' } );
+        })
+    });
+
+    describe('creating', function(){
+        it('should create with defaults', function(){
+            var schemaRegistry = SchemaRegistry.create();
+            var schema = {
+                id: '/component/flower',
+                properties:{
+                    name: { type:'string', 'default':'daisy' },
+                    colour: { type:'string', 'default':'red' },
+                    height: { type:'number', 'default':1.0 }
+                }
+            };
+            var defaults = { name:'daisy', colour:'red', height:1.0 };
+
+            var def = ComponentDef.create( schema, null, defaults );
+            ComponentDef.isComponentDef(def).should.equal(true);
+            var com = def.create();
+
+            com.toJSON().should.deep.equal({
+                name:'daisy', colour: 'red', height: 1.0
+            });
+        });
+    });
+});
