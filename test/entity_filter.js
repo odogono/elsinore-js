7363ce24f167705384f003730ce6df7e8051f2f2
Alex Veenendaal <alex@opendoorgonorth.com>
2015-06-17 13:12:12 +0100

    removing now unneeded code from EntityFilter

--- test/entity_filter.js
+++ test/entity_filter.js
@@ -6,289 +6,153 @@ var Sinon = require('sinon');
 
 module.exports = function( test, Common, Elsinore, EntitySet ){
 
-    var Component = Elsinore.Component;
-    var Entity = Elsinore.Entity;
-    var EntityFilter = Elsinore.EntityFilter;
-    var Registry = Elsinore.Registry;
+    let BitField = Elsinore.BitField;
+    let Component = Elsinore.Component;
+    let Entity = Elsinore.Entity;
+    let EntityFilter = Elsinore.EntityFilter;
+    let Registry = Elsinore.Registry;
 
-
-
-test('creating a filter by passing multiple component ids', t => {
-    let f = EntityFilter.create( EntityFilter.INCLUDE, [Components.Animal, Components.Doctor, Components.Flower] );
-    t.deepEqual( f.toArray(), [EntityFilter.INCLUDE, [Components.Animal, Components.Doctor, Components.Flower]] );
-    t.end();
-});
-
-
-
-test('creating an array filter by passing an array', t => {
-    let f = EntityFilter.create( EntityFilter.NONE, [Components.Mineral, Components.Animal] );
-    f.add( EntityFilter.INCLUDE, [Components.Animal, Components.Doctor, Components.Flower] );
-
-    t.equals( f.size(), 2 );
-    // t.equals( f.entityFilterType, EntityFilter.ARRAY );
-    
-    t.deepEqual( f.toArray(),[
-        [EntityFilter.NONE, [Components.Animal, Components.Mineral]],
-        [EntityFilter.INCLUDE, [Components.Animal, Components.Doctor, Components.Flower]]
-        ]);
-
-    t.end();
-});
-
-test('filters will have identical hashes', t => {
-    var hashFn = EntityFilter.hash;
-    t.equals(
-        hashFn( EntityFilter.create( EntityFilter.INCLUDE, Components.Animal ) ),
-        hashFn( EntityFilter.create( EntityFilter.INCLUDE, Components.Animal ) )
-        );
-    t.notEqual(
-        hashFn( EntityFilter.create( EntityFilter.INCLUDE, Components.Animal ) ),
-        hashFn( EntityFilter.create( EntityFilter.EXCLUDE, Components.Animal ) )
-        );
-
-
-
-    t.end();
-});
 
 
 test('an default filter will accept an entity', t => {
-    var e = Entity.create();
-    var f = EntityFilter.create();
-    t.ok( f.accept(e), 'the filter accepts an entity by default' );
+    let e = Entity.create();
+    t.ok( EntityFilter.accept(EntityFilter.ALL, toBitField(e), toBitField() ), 
+        'the filter accepts an entity by default' );
     t.end();
 });
 
 test('will reject entities without components', t => {
-    var e = Entity.create();
-    var f = EntityFilter.create(EntityFilter.SOME);
-    t.notOk( f.accept(e), 'the filter rejects the entity without components');
+    let e = Entity.create();
+    t.notOk( 
+        EntityFilter.accept( EntityFilter.SOME, e.getComponentBitfield() ), 
+        'the filter rejects the entity without components');
     t.end();
 });
 
 test('will accept entities with one of the components', t => {
-    var f = EntityFilter.create( EntityFilter.ANY, [Components.Animal, Components.Doctor] );
+    let type = EntityFilter.ANY;
+    let bitField = toBitField( Components.Animal, Components.Doctor );
 
-    t.ok( f.accept( createEntity( Components.Animal ) ) );
-    t.notOk( f.accept( createEntity( Components.Mineral ) ) );
-    t.ok( f.accept( createEntity( Components.Doctor ) ) );
-    t.ok( f.accept( createEntity( Components.Robot, Components.Animal ) ) );
+    t.ok( EntityFilter.accept(type, toBitField(Components.Animal), bitField) );
+    t.notOk( EntityFilter.accept(type, toBitField(Components.Mineral), bitField) );
+    t.ok( EntityFilter.accept(type, toBitField(Components.Doctor), bitField) );
+    t.ok( EntityFilter.accept(type, toBitField(Components.Robot, Components.Animal), bitField) );
 
     t.end();
 });
 
 test('reject an entity which does not have a specific component', t => {
-    var e = Entity.create();
-    var f = EntityFilter.create(EntityFilter.ALL, Components.Flower );
+    let e = Entity.create();
+    let type = EntityFilter.ALL;
+    let bitField = toBitField( Components.Flower );
 
-    t.notOk( f.accept(e), 'filter rejects because the component is missing');
+    t.notOk( EntityFilter.accept(type, toBitField(e), bitField ), 
+        'filter rejects because the component is missing');
     e.addComponent( createComponent( Components.Flower ) );
     e.addComponent( createComponent( Components.Robot ) );
-    t.ok( f.accept(e), 'filter accepts because the component is present');
+    t.ok( EntityFilter.accept(type, toBitField(e), bitField ), 
+        'filter accepts because the component is present');
+
     t.end();
 });
 
 test('reject an entity which does not have the specific components', t => {
-    var e = Entity.create();
-    var f = EntityFilter.create( EntityFilter.ALL, [Components.Mineral, Components.Vegetable] );
+    let e = Entity.create();
+    let type = EntityFilter.ALL;
+    let bitField = toBitField( Components.Mineral, Components.Vegetable );
 
     e.addComponent( createComponent( Components.Animal ) );
     e.addComponent( createComponent( Components.Mineral ) );
-    t.notOk( f.accept(e) );
+    t.notOk( EntityFilter.accept(type, toBitField(e), bitField) );
     e.addComponent( createComponent( Components.Vegetable ) );
-    t.ok( f.accept(e, true) );
+    t.ok( EntityFilter.accept(type, toBitField(e), bitField) );
 
     t.end();
 });
 
 test('accepts an entity which has some of the components', t => {
-    var e = Entity.create();
-    var f = EntityFilter.create( EntityFilter.ANY, [Components.Animal, Components.Mineral, Components.Vegetable] );
+    let e = Entity.create();
+    let type = EntityFilter.ANY;
+    let bitField = toBitField( Components.Animal, Components.Mineral, Components.Vegetable );
     
-    t.notOk( f.accept(e) );
+    t.notOk( EntityFilter.accept(type, toBitField(e), bitField) );
     e.addComponent( createComponent( Components.Robot ) );
-    t.notOk( f.accept(e) );
-    // log.debug('--- should fail because none');
+    t.notOk( EntityFilter.accept(type, toBitField(e), bitField) );
     e.addComponent( createComponent( Components.Animal ) );
-    t.ok( f.accept(e), 'has one of the optional components' );
+    t.ok( EntityFilter.accept(type, toBitField(e), bitField), 'has one of the optional components' );
     t.end();
 });
 
 test('rejects an entity which has any of the components', t => {
-    var e = Entity.create();
-    var f = EntityFilter.create( EntityFilter.NONE, Components.Vegetable );
+    let e = Entity.create();
+    let type = EntityFilter.NONE;
+    let bitField = toBitField( Components.Vegetable );
 
-    t.ok( f.accept(e) );
+
+    t.ok( EntityFilter.accept(type, toBitField(e), bitField) );
     e.addComponent( createComponent( Components.Animal ) );
-    t.ok( f.accept(e) );
+    t.ok( EntityFilter.accept(type, toBitField(e), bitField) );
     e.addComponent( createComponent( Components.Vegetable ) );
-    t.notOk( f.accept(e) );
+    t.notOk( EntityFilter.accept(type, toBitField(e), bitField) );
 
     t.end();
 });
 
-test('rejects an entity which has any of the components with multiple filters', t => {
-    var f = EntityFilter.create(
-        [[EntityFilter.NONE, Components.Vegetable],
-        [EntityFilter.ALL, Components.Animal]] );
 
-    var e = Entity.create();
-    t.notOk( f.accept(e) );
+// test('transform will copy an incoming entity', t => {
+//     let te, f = EntityFilter.create();
+//     let e = createEntity( Components.Mineral, Components.Vegetable, Components.Doctor );
     
-    e.addComponent( createComponent( Components.Animal ) );
-    t.ok( f.accept(e) );
+//     e.marked = true;
+//     te = f.transform(e);
 
-    e.addComponent( createComponent( Components.Vegetable ) );
-    t.notOk( f.accept(e) );
+//     t.notOk( te.marked );
+//     t.ok( te.Mineral );
+//     t.ok( te.Vegetable );
+//     t.ok( te.Doctor );
+//     t.end();
+// });
 
-    t.end();
-});
+// test('transform will include only specified components on an entity', t => {
+//     var e = createEntity( Components.Mineral, Components.Vegetable, Components.Robot );
 
+//     var f = EntityFilter.create( EntityFilter.INCLUDE, [Components.Animal, Components.Robot, Components.Doctor] );
 
+//     t.ok( e.Robot, 'entity will have Robot component' );
+//     t.ok( e.Mineral, 'entity will have Mineral component' );
 
-test('transform will copy an incoming entity', t => {
-    let te, f = EntityFilter.create();
-    let e = createEntity( Components.Mineral, Components.Vegetable, Components.Doctor );
+//     var te = f.transform( e );
+//     t.equal( e.id, te.id, 'transformed entity id will be the same' );
+//     t.ok( te.Robot, 'transformed entity will have Robot component' );
+//     t.notOk( te.Mineral, 'transformed entity will not have Mineral component' );
     
-    e.marked = true;
-    te = f.transform(e);
+//     t.end();
+// });
 
-    t.notOk( te.marked );
-    t.ok( te.Mineral );
-    t.ok( te.Vegetable );
-    t.ok( te.Doctor );
-    t.end();
-});
+// test('transform will exclude specified components on an entity', t => {
+//     var e = createEntity( Components.Mineral, Components.Vegetable, Components.Robot );
+//     var f = EntityFilter.create( EntityFilter.EXCLUDE, Components.Vegetable );
 
-test('transform will include only specified components on an entity', t => {
-    var e = createEntity( Components.Mineral, Components.Vegetable, Components.Robot );
+//     var te = f.transform( e );
+//     t.equal( e.id, te.id, 'transformed entity id will be the same' );
+//     t.ok( te.Mineral, 'transformed entity will have Mineral component' );
+//     t.notOk( te.Vegetable, 'transformed entity will not have Vegetable component' );
 
-    var f = EntityFilter.create( EntityFilter.INCLUDE, [Components.Animal, Components.Robot, Components.Doctor] );
-
-    t.ok( e.Robot, 'entity will have Robot component' );
-    t.ok( e.Mineral, 'entity will have Mineral component' );
-
-    var te = f.transform( e );
-    t.equal( e.id, te.id, 'transformed entity id will be the same' );
-    t.ok( te.Robot, 'transformed entity will have Robot component' );
-    t.notOk( te.Mineral, 'transformed entity will not have Mineral component' );
-    
-    t.end();
-});
-
-test('transform will exclude specified components on an entity', t => {
-    var e = createEntity( Components.Mineral, Components.Vegetable, Components.Robot );
-    var f = EntityFilter.create( EntityFilter.EXCLUDE, Components.Vegetable );
-
-    var te = f.transform( e );
-    t.equal( e.id, te.id, 'transformed entity id will be the same' );
-    t.ok( te.Mineral, 'transformed entity will have Mineral component' );
-    t.notOk( te.Vegetable, 'transformed entity will not have Vegetable component' );
-
-    t.end();
-});
+//     t.end();
+// });
 
 
-test('transform all on a single component', t => {
-    var e = createEntity( Components.Mineral, Components.Vegetable, Components.Robot );
-    var f = EntityFilter.create( EntityFilter.ALL, Components.Vegetable );
+// test('transform all on a single component', t => {
+//     var e = createEntity( Components.Mineral, Components.Vegetable, Components.Robot );
+//     var f = EntityFilter.create( EntityFilter.ALL, Components.Vegetable );
 
-    var te = f.transform( e );
-    t.ok( te.Mineral, 'transformed entity will have Mineral component' );
-    t.ok( te.Robot, 'transformed entity will have Robot component' );
-    t.ok( te.Vegetable, 'transformed entity will not have Vegetable component' );
+//     var te = f.transform( e );
+//     t.ok( te.Mineral, 'transformed entity will have Mineral component' );
+//     t.ok( te.Robot, 'transformed entity will have Robot component' );
+//     t.ok( te.Vegetable, 'transformed entity will not have Vegetable component' );
 
-    t.end();
-});
-
-test('creating a filter with a custom accept function', t => {
-
-    var f = EntityFilter.create( function accept(entity,options){
-        if( entity.Robot && entity.Robot.get('age') > 40  ){
-            return true;
-        }
-        return false;
-    });
-
-
-    t.notOk( f.accept( createEntity( {_c:Components.Robot, age:32} )));
-    t.ok( f.accept( createEntity( {_c:Components.Robot, age:41} )));
-    t.notOk( f.accept( createEntity( Components.Animal ) ));
-
-    t.end();
-});
-
-test.skip('creating a filter with a standard and a custom function', t => {
-    
-    var f = EntityFilter.create( 
-        EntityFilter.ALL, Components.Robot,
-        function accept(entity,options){
-            if( entity.Robot.get('age') > 40  ){
-                return true;
-            }
-            return false;
-        });
-
-
-    t.ok( f.accept( createEntity( {_c:Components.Robot, age:41} )), 'accepting a robot aged 41');
-    t.notOk( f.accept( createEntity( {_c:Components.Robot, age:32} )), 'not accepting a robot aged 32');
-    t.notOk( f.accept( createEntity( {_c:Components.Animal, age:51} )), 'not accepting an animal age 51');
-
-    t.end();
-});
-
-test('adding a user function to the filter', t => {
-    let f = EntityFilter.create();
-    f.add( EntityFilter.ALL, Components.Robot );
-    f.add( entity => entity.Robot.get('age') > 500 );
-
-    t.ok( f.accept( createEntity( {_c:Components.Robot, age:600} )), 'accepting a robot aged 600');
-    t.notOk( f.accept( createEntity( {_c:Components.Robot, age:32} )), 'not accepting a robot aged 32');
-    t.notOk( f.accept( createEntity( {_c:Components.Animal, age:51} )), 'not accepting an animal age 51');
-
-    t.end();
-});
-
-test('attribute filter filters attributes', t => {
-    var f = EntityFilter.create( EntityFilter.ATTRIBUTES, Components.Robot, {colour:'blue'} );
-
-    t.ok(
-        f.accept( createEntity( {_c:Components.Robot, colour:'blue'})),
-        'accepting a blue robot' );
-    t.notOk(
-        f.accept( createEntity( {_c:Components.Robot, colour:'white'})),
-        'not accepting a white robot' );
-    t.end();
-});
-
-test('iterator iterates allowable entities from a source', t => {
-    var entity;
-    var it;
-    var count;
-    var f = EntityFilter.create( EntityFilter.ANY, [Components.Animal, Components.Mineral, Components.Vegetable] );
-    var entitySet = createEntitySet();
-    // Common.logEvents( es );
-
-    entitySet.addEntity( createEntity( Components.Animal ) );
-    entitySet.addEntity( createEntity( Components.Flower ) );
-    entitySet.addEntity( createEntity( Components.Mineral ) );
-    entitySet.addEntity( createEntity( Components.Vegetable ) );
-    entitySet.addEntity( createEntity( Components.Robot ) );
-
-    // use the entitySet as the source of entities
-    it = f.iterator( entitySet );
-    count = 0;
-
-    while( (entity = it.next().value) ){
-        count++;
-    }
-
-    t.equal( count, 3, 'three entities should have been returned' );
-
-    t.end();
-});
-
+//     t.end();
+// });
 
 
 
@@ -320,6 +184,19 @@ var ComponentIIdToObject = _.reduce( ComponentDefs, function(memo,val,key){
     return memo;
 },[]);
 
+
+function toBitField( componentIIds ){
+    let args = _.toArray( arguments );
+    if( args.length <= 0 ){
+        return BitField.create();
+    }
+    
+    if( Entity.isEntity(componentIIds) ){
+        return componentIIds.getComponentBitfield();
+    }
+    
+    return BitField.create( args );
+}
 
 function createEntity( componentIIds ){
     var i,len,com;
