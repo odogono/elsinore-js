6613ae7b09a9070ff79ffd8f8d579d3093644b78
Alex Veenendaal <alex@opendoorgonorth.com>
2015-08-07 14:14:47 +0100

    updated build to include envify and unreachable-branch -transform. the latter makes it possible to cut out code that is never reached.

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,23 @@ var Path = require('path');
 
 var _ = require('underscore');
 var Aliasify = require('aliasify');
+var Babelify = require("babelify");
 var Browserify = require('browserify');
 var Buffer = require('vinyl-buffer');
 var Del = require('del');
+var Envify = require('envify');
 var Gulp = require('gulp');
 var Jslint = require('gulp-jslint');
 var Source = require('vinyl-source-stream');
 var Uglify = require('gulp-uglify');
+var UnreachableBranchTransform = require('unreachable-branch-transform');
 var Webserver = require('gulp-webserver');
 
 var packageObj = require('./package.json');
 var vendorDependencies = _.keys( packageObj.dependencies );
+
+var UGLIFY_VENDOR_LIBS = false;
+var UGLIFY_LIB = true;
 
 
 var paths = {
@@ -68,16 +74,35 @@ Gulp.task('lint', function(){
 
 Gulp.task('build.vendor', function(){
     // console.log( vendorDependencies );
-    return Browserify({
+    var b = Browserify({
             noparse: vendorDependencies,
             debug: true
         })
-    .on('error', handleErrors)
-    .require(vendorDependencies)
-    .bundle()
-    .pipe(Source( Path.basename(pathExternals) ))
-    .pipe(Buffer()).pipe(Uglify())
-    .pipe(Gulp.dest( Path.dirname(pathExternals) ));
+    .on('error', handleErrors);
+
+    b.transform( Babelify );
+
+    b.transform( Envify, {
+        NODE_ENV: 'development'
+    });
+
+    b.transform( UnreachableBranchTransform );
+
+    // backbone will include jquery if required
+    b.exclude('jquery');
+
+    b.require(vendorDependencies);
+
+    b = b.bundle()
+        .pipe(Source( Path.basename(pathExternals) ));
+
+    if( UGLIFY_VENDOR_LIBS ){
+        b = b.pipe(Buffer()).pipe(Uglify());
+    }
+
+    b.on('error', function (err) { console.log("Error : " + err.message); })
+        .pipe(Gulp.dest( Path.dirname(pathExternals) ));
+    return b;
 });
 
 
@@ -87,6 +112,7 @@ Gulp.task('build.lib', function(cb){
             // entries:[ './lib/index.js' ],
             debug: false
         });
+
     b.require('./lib/index.js', {expose:'elsinore'});
     // browserify external doesn't currently appear to work with an array,
     // so we must manually apply each one
@@ -94,13 +120,25 @@ Gulp.task('build.lib', function(cb){
         b.external( lib );
     });
 
-    b.bundle()
+    b.transform( Babelify );
+
+    b.transform( Envify, {
+        NODE_ENV: 'development'
+    });
+
+    b.transform( UnreachableBranchTransform );
+    
+
+    b = b.bundle()
         .on('error', function(err){
             return cb('error in bundle: ' + err.message );
         })
-        .pipe(Source( Path.basename(paths.dist.lib) ))
-        .pipe(Buffer()).pipe(Uglify())
-        .pipe(Gulp.dest( Path.dirname(paths.dist.lib) ))
+        .pipe(Source( Path.basename(paths.dist.lib) ));
+    if( UGLIFY_LIB ){
+        b = b.pipe(Buffer()).pipe(Uglify());
+    }
+        //.pipe(Buffer()).pipe(Uglify())
+    b = b.pipe(Gulp.dest( Path.dirname(paths.dist.lib) ))
         .on('finish', function(){
             return cb();
         });
@@ -114,6 +152,8 @@ Gulp.task('test.browser.build', function(cb){
         debug:true
         });
 
+    b.transform( Babelify );
+
     // transform certain requires present in the tests
     // into something more browser friendly.
     // this is critical to allowing us to run the same
@@ -125,6 +165,12 @@ Gulp.task('test.browser.build', function(cb){
         },
         verbose: true
     });
+
+    b.transform( Envify, {
+        NODE_ENV: 'development'
+    });
+
+    b.transform( UnreachableBranchTransform );
 
     // browserify external doesn't currently appear to work with an array,
     // so we must manually apply each one
