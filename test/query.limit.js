38710f0c64d0e43cc7f62d85d7c6d795e406b528
Alex Veenendaal <alex@opendoorgonorth.com>
2015-08-11 11:58:12 +0100

    the Query.limit command is now folded into the nearest entityFilter command as options. If no entityFilter command exists, an empty one is added.

--- test/query.limit.js
+++ test/query.limit.js
@@ -25,7 +25,26 @@ export default function run( test, Common, Elsinore, EntitySet ){
             
             t.deepEqual( result, [11,12,13,14,15] );
             t.end();
-        });
+        })
+        .catch( err => { log.debug('t.error: ' + err ); log.debug( err.stack );} )
+    });
+
+    test('apply offset and limit to a select', t => {
+        initialiseEntitySet().then( ([registry,entitySet]) => {
+            let result = Query.create( registry, [
+                Query.all('/component/username'),
+                Query.limit( 2 ),
+                // Query.pluck( null, 'eid', {unique:true})
+            ]);
+            
+            t.deepEqual( result.commands.length, 1 );
+            result = entitySet.query( result );
+
+            t.equals( result.size(), 2, 'only two entities returned' );
+            
+            t.end();
+        })
+        .catch( err => { log.debug('t.error: ' + err ); log.debug( err.stack );} )
     });
     
     function initialiseEntitySet(){
