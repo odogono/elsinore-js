d439ee303ad9129e4cdab9ec6fa84e0012282d5e
Alex Veenendaal <alex@opendoorgonorth.com>
2017-04-06 10:19:14 +0100

    package/rollup upgrades

--- rollup.config.es.js
+++ rollup.config.es.js
@@ -4,47 +4,44 @@ import CommonJS from 'rollup-plugin-commonjs';
 import Flow from 'rollup-plugin-flow';
 import NodeResolve from 'rollup-plugin-node-resolve';
 import Replace from 'rollup-plugin-replace';
+import Uglify from 'rollup-plugin-uglify';
 
+const pkg = JSON.parse(readFileSync('package.json', 'utf-8'));
+const environment = process.env.NODE_ENV || 'development';
+const isProduction = environment === 'production';
 
-const pkg = JSON.parse( readFileSync( 'package.json', 'utf-8' ) );
-
-
-const banner = readFileSync( 'src/banner.js', 'utf-8' )
-	.replace( '${version}', pkg.version )
-	.replace( '${time}', new Date() )
+const banner = readFileSync('src/banner.js', 'utf-8').replace('${version}', pkg.version).replace('${time}', new Date());
 
 export default {
     banner,
     entry: 'src/index.js',
-    dest: 'dist/elsinore.es2015.js',
+    dest: isProduction ? 'dist/elsinore.es2015.min.js' : 'dist/elsinore.es2015.js',
     sourceMap: true,
-    plugins:[
-        Replace({
-            'process.env.NODE_ENV': JSON.stringify('development')
-        }),
+    plugins: [
+        Replace({ 'process.env.NODE_ENV': JSON.stringify(environment) }),
         Flow(),
-        CommonJS({
-            include: 'node_modules/**'
-			// include: [
-            //     'node_modules/underscore/underscore.js',
-            //     'node_modules/deep-equal/index.js'
-            // ],
-		}),
-        NodeResolve({preferBuiltins:true, jsnext:true,main:true}),
         Babel({
             babelrc: false,
-			sourceMap: true,
+            sourceMap: true,
             exclude: 'node_modules/**',
-            presets: [
-                "es2015-rollup"
-            ],
+            presets: [ 'es2015-rollup' ],
             plugins: [
                 // "external-helpers",
                 // "transform-es2015-destructuring",
-                "transform-inline-environment-variables",
-                "transform-object-rest-spread",
-                "transform-es2015-shorthand-properties", // uglifyjs has been having problems with this
-            ]
-        })
-    ]
+                'transform-inline-environment-variables',
+                'transform-object-rest-spread',
+                // uglifyjs has been having problems with this
+                'transform-es2015-shorthand-properties',
+            ],
+        }),
+        NodeResolve({ preferBuiltins: true, jsnext: true, main: true }),
+        CommonJS({
+            // include: [
+            //     'node_modules/underscore/underscore.js',
+            //     'node_modules/deep-equal/index.js'
+            // ],
+            include: 'node_modules/**',
+        }),
+        isProduction && Uglify(),
+    ],
 }
--- rollup.config.js
+++ rollup.config.js
@@ -4,49 +4,41 @@ import CommonJS from 'rollup-plugin-commonjs';
 import Flow from 'rollup-plugin-flow';
 import NodeResolve from 'rollup-plugin-node-resolve';
 import Replace from 'rollup-plugin-replace';
+import Uglify from 'rollup-plugin-uglify';
 
+const pkg = JSON.parse(readFileSync('package.json', 'utf-8'));
 
-const pkg = JSON.parse( readFileSync( 'package.json', 'utf-8' ) );
+const environment = process.env.NODE_ENV || 'development';
+const isProduction = environment === 'production';
 
-
-const banner = readFileSync( 'src/banner.js', 'utf-8' )
-	.replace( '${version}', pkg.version )
-	.replace( '${time}', new Date() )
+const banner = readFileSync('src/banner.js', 'utf-8').replace('${version}', pkg.version).replace('${time}', new Date());
 
 export default {
     banner,
     entry: 'src/index.js',
-    format: 'iife',
-    moduleName: 'elsinore',
-    dest: 'dist/elsinore.js',
+    format: 'cjs',
+    // moduleName: 'elsinore',
+    dest: isProduction ? 'dist/elsinore.min.js' : 'dist/elsinore.js',
     sourceMap: true,
-    plugins:[
-        Replace({
-            'process.env.NODE_ENV': JSON.stringify('development')
-        }),
+    plugins: [
+        Replace({ 'process.env.NODE_ENV': JSON.stringify(environment) }),
         Flow(),
-        CommonJS({
-            include: 'node_modules/**'
-			// include: [
-            //     'node_modules/underscore/underscore.js',
-            //     'node_modules/deep-equal/index.js'
-            // ],
-		}),
-        NodeResolve({preferBuiltins:true, jsnext:true,main:true}),
         Babel({
             babelrc: false,
-			sourceMap: true,
+            sourceMap: true,
             exclude: 'node_modules/**',
-            presets: [
-                "es2015-rollup"
-            ],
+            presets: [ 'es2015-rollup' ],
             plugins: [
                 // "external-helpers",
                 // "transform-es2015-destructuring",
-                "transform-inline-environment-variables",
-                "transform-object-rest-spread",
-                "transform-es2015-shorthand-properties", // uglifyjs has been having problems with this
-            ]
-        })
-    ]
+                'transform-inline-environment-variables',
+                'transform-object-rest-spread',
+                // uglifyjs has been having problems with this
+                'transform-es2015-shorthand-properties',
+            ],
+        }),
+        NodeResolve({ preferBuiltins: true, jsnext: true, main: true }),
+        CommonJS({}),
+        isProduction && Uglify(),
+    ],
 }
