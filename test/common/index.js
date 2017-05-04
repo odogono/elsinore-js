c342795536072908967d1957a01289709ae617dd
Alex Veenendaal <alex@opendoorgonorth.com>
2017-05-04 06:29:31 +0100

    got rid of some unused packages

--- test/common/index.js
+++ test/common/index.js
@@ -1,11 +1,8 @@
 import _ from 'underscore';
 
 import Fs from 'fs';
-import Es from 'event-stream';
 import Path from 'path';
 import Util from 'util';
-
-import Sh from 'shelljs';
 
 const rootDir = Path.join( Path.dirname(__filename), '../../' );
 const fixtureDir = Path.join( rootDir, 'test', 'fixtures' );
@@ -54,22 +51,6 @@ export {Dispatch as Dispatch};
 const Log = createLog('Test');
 
 
-export function pathVar( path, clear ){
-    path = Path.join( varDir, path );
-    if( clear ){ Sh.rm('-rf', path ); }
-    Sh.mkdir('-p', path );
-    return path;
-}
-
-export function pathVarFile( path, clear ){
-    path = Path.join( varDir, path );
-    if( clear ){ 
-        // log.debug('clearing ' + path );
-        Sh.rm('-rf', path ); 
-    }
-    Sh.mkdir('-p', Path.dirname(path) );
-    return path;
-}
 
 // compile a map of schema id(uri) to schema
 export function loadComponents(options={}){
@@ -148,13 +129,6 @@ export async function initialiseRegistry( doLogEvents ){
     return registry;
 }
 
-
-export function createFixtureReadStream( fixturePath ){
-    let path = Path.join( fixtureDir, fixturePath );
-    return Fs.createReadStream( path, { encoding: 'utf-8' })
-        .pipe(Es.split())
-        .pipe(Es.parse());
-}
 
 export function loadFixture( fixturePath ){
     let path = Path.join( fixtureDir, fixturePath );
