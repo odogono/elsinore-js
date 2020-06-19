2d62d2a84a270cdada492ce9865f839319089f01
Alex Veenendaal <alex@opendoorgonorth.com>
2020-06-19 07:30:18 +0100

    dehydrate/rehydrate

--- src/util/json.ts
+++ src/util/json.ts
@@ -1,3 +1,6 @@
+import Jsonpointer from 'jsonpointer';
+import {isObject} from './is';
+
 /**
  * Safe parsing of json data
  */
@@ -31,4 +34,60 @@ export function stringify(obj:any, space?:(number|string)) : string {
         },
         space
     );
+}
+
+
+export type DehydrateResult = [ string, any ];
+
+/**
+ * takes an array of JSON pointer paths to values and produces an
+ * object
+ * 
+ * @param parts 
+ * @param obj 
+ */
+export function rehydrate( parts:DehydrateResult[], obj:object ){
+    for( const [ptr,val] of parts ){
+        Jsonpointer.set(obj, ptr, val);
+    }
+    return obj;
+}
+
+
+
+
+
+/**
+ * takes an object and produces an array of JSON pointer paths to values
+ * 
+ * @param obj 
+ */
+export function dehydrate( obj:object ):DehydrateResult[] {
+    let result:DehydrateResult[] = [];
+
+    dehydrateWalk( obj, [], result );
+    
+    return result;
+}
+
+
+function dehydrateWalk( obj:any, path:string[], result:any[] ){
+    if( Array.isArray(obj) ){
+        for( let ii=0;ii<obj.length;ii++ ){
+            let spath = [...path, String(ii) ];
+            [spath,result] = dehydrateWalk( obj[ii], spath, result );
+        }
+    }
+    else if( isObject(obj) ){
+        for( const key in obj ){
+            if( !obj.hasOwnProperty(key) ){ continue; }
+            let spath = [...path, key];
+            [spath,result] = dehydrateWalk( obj[key], spath, result );
+        }
+    }
+    else {
+        result.push( [ '/' + path.join('/'), obj ] );
+    }
+
+    return [path, result];
 }
