e3dfd0a51f9a3b1e1ab4bdbad4c7636c7ff415ec
Alex Veenendaal <alex@opendoorgonorth.com>
2021-04-27 20:14:54 +0100

    further refinements to tokenizer

--- src/query/tokenizer.ts
+++ src/query/tokenizer.ts
@@ -1,22 +1,20 @@
-let debugLog = false;
-
+import { parseJSON } from "@odgn/utils";
 
 const MODE_IDLE = 0;
-const MODE_COMMENT = 1<<0;
-const MODE_MULTI_COMMENT = 1<<1;
-const MODE_MULTI_QUOTE = 1<<2;
-const MODE_QUOTE = 1<<3;
-const MODE_MAYBE_QUOTE = 1<<4;
-const MODE_VALUE = 1<<5;
-// const MODE_MAYBE_VALUE = 1<<6;
+const MODE_COMMENT = 1 << 0;
+const MODE_MULTI_COMMENT = 1 << 1;
+const MODE_MULTI_QUOTE = 1 << 2;
+const MODE_QUOTE = 1 << 3;
+const MODE_MAYBE_QUOTE = 1 << 4;
+const MODE_VALUE = 1 << 5;
 
-function set(flag,val){
+function set(flag, val) {
     return flag | val;
 }
-function isSet(flag,val){
+function isSet(flag, val) {
     return (flag & val) === val;
 }
-function unset(flag,val){
+function unset(flag, val) {
     return flag & ~val;
 }
 
@@ -27,69 +25,61 @@ interface Context {
     length: number;
     output: any[];
     line: number;
-    // position: number;
     markPosition: number;
     linePosition: number;
     charBuffer: string[];
     mode: number;
     endChar: string;
-    // withinQuote: boolean;
-    // maybeWithinQuote: boolean;
-    // withinMultiQuote: boolean;
-    // withinComment: boolean;
-    // withinMultiComment: boolean;
+    mapCount: number;
+    expectKey: boolean;
 }
 
-export function identity(){
-    return 'tokenizer';
-}
 
 export interface TokenizeOptions {
     returnValues?: boolean;
 }
 
-export function tokenizeString(data:string, options:TokenizeOptions = {}) {
+/**
+ * 
+ * @param data 
+ * @param options 
+ */
+export function tokenizeString(data: string, options: TokenizeOptions = {}) {
     const returnValues = options.returnValues ?? false;
     let context = createContext();
 
-    if( !data.endsWith('\n') ){
+    if (!data.endsWith('\n')) {
         data = data + '\n';
     }
 
     context = tokenize(context, data);
 
-    return returnValues ? context.output.map( e => e[0] ) : context.output;
+    return returnValues ? context.output.map(e => e[0]) : context.output;
 }
 
 /**
  * Entry point for parsing a new string
  */
-export function tokenize(context:Context, input:string):Context {
+export function tokenize(context: Context, input: string): Context {
     context = context || createContext();
     context.length = context.pos + input.length;
 
-    // const len = (context.pos + input.length);
-    // console.log('dammit', context.pos, context.length );
-
-    // let it = 0;
-    while (context.pos < context.length ) {
-        // it++;
+    while (context.pos < context.length) {
         context = process(context, input);
-        // console.log('proc', context.pos, len );
-        // if( it > 100 ){ throw new Error('over')}
     }
 
-    // context.position += input.length;
     return context;
 }
 
-function process(context:Context, input:string):Context {
+function process(context: Context, input: string): Context {
     let { pos, length, offset, mode,
         markPosition,
-        output, 
+        output,
         endChar,
+        mapCount,
+        expectKey,
         buffer, charBuffer, line, linePosition = 0 } = context;
-    
+
     let cpos = 0;
     for (pos; pos < length; cpos++, pos++) {
         let char = input.charAt(cpos);
@@ -102,16 +92,15 @@ function process(context:Context, input:string):Context {
         charBuffer[0] = char;
 
         const isNewline = char === '\n';
-        
 
-        
-        if ( isSet(mode,MODE_MAYBE_QUOTE)) {
+
+
+        if (isSet(mode, MODE_MAYBE_QUOTE)) {
             // console.log('maybequote', char, charBuffer, char !== "'", charBuffer[1] === "'" );
             let clear = false;
-            if( char === '*' || char === '/' || char === "'" ){
-                // console.log("GO", char, charBuffer);
+            if (char === '*' || char === '/' || char === "'") {
                 if (char === '*' && charBuffer[1] === '/') {
-                    mode = set(mode,MODE_MULTI_COMMENT);
+                    mode = set(mode, MODE_MULTI_COMMENT);
                     clear = true;
                 }
                 else if (char === '/' && charBuffer[1] === '/') {
@@ -119,58 +108,76 @@ function process(context:Context, input:string):Context {
                     clear = true;
                 }
                 else if (char === "'" && charBuffer[1] === "'" && charBuffer[2] === "'") {
-                    mode = set(mode,MODE_MULTI_QUOTE);
-                    
+                    mode = set(mode, MODE_MULTI_QUOTE);
+
                     offset = linePosition;
-                    // Log('process', 'multiquote offset', offset);
                     clear = true;
                 }
-                
+
             }
-            else if( char !== "'" && charBuffer[1] === "'" && charBuffer[2] === "'" ){
-                // console.log('oh snap', char, charBuffer, buffer););
-                output.push(["",markPosition,line]);
+            else if (char !== "'" && charBuffer[1] === "'" && charBuffer[2] === "'") {
+                output.push(["", markPosition, line]);
                 clear = true;
             }
-            else if( char !== "'" && charBuffer[1] === "'" ){
+            else if (char !== "'" && charBuffer[1] === "'") {
                 mode = set(mode, MODE_QUOTE);
-                mode = unset(mode, MODE_MAYBE_QUOTE );
+                mode = unset(mode, MODE_MAYBE_QUOTE);
                 offset = linePosition;
                 markPosition = pos;
-                // char = '';
                 endChar = "'";
-                // clear = false;
                 buffer = char;
-                // console.log('dammit', char, charBuffer, buffer);
                 continue;
             }
             else {
-                // console.log('start val', char, pos);
-                mode = set(mode,MODE_VALUE);
-                mode = unset(mode, MODE_MAYBE_QUOTE );
+                // console.log('start val', {char, pos, mapCount, expectKey, last:charBuffer[1]}, buffer);
+                mode = set(mode, MODE_VALUE);
+                mode = unset(mode, MODE_MAYBE_QUOTE);
+                endChar = char === '"' ? char : '';
+                
                 offset = linePosition;
+                if (mapCount > 0 && charBuffer[1] === ':' && expectKey ){ //&& (char === '"' || char === "'")) {
+                    // console.log(`map delim (${char}) (${buffer})`, {expectKey, mapCount});
+                    buffer = '';
+                    markPosition++;
+                    expectKey = !expectKey;
+                }
+                else if( mapCount > 0 ){
+                    // console.log(`nope delim (${char}) (${buffer})`, {expectKey, mapCount});
+                }
             }
 
             if (clear) {
-                mode = unset(mode, MODE_MAYBE_QUOTE );
+                mode = unset(mode, MODE_MAYBE_QUOTE);
                 // mode = MODE_IDLE;
                 // maybeWithinQuote = false;
                 buffer = '';
                 continue;
             }
 
-            if( isNewline || char === ' ' ){
-                // console.log('endof', markPosition, buffer );
-                output.push([
-                    parseValue(buffer.trimEnd()),
-                    markPosition,
-                    line
-                ]);
+            if (isNewline || char === ' ') {
+                // console.log('endof', { markPosition, mapCount }, buffer);
+                if (mapCount === 0) {
+                    
+                    output.push([
+                        parseValue(buffer.trimEnd()),
+                        markPosition,
+                        line
+                    ]);
+                }
                 mode = unset(mode, MODE_MULTI_QUOTE | MODE_VALUE);
                 // maybeWithinQuote = false;
                 buffer = '';
             }
             else {
+                if (char === '{') {
+                    mapCount++;
+                    expectKey = true;
+                    // console.log('inc', {mapCount, char, pos})
+                } else if (char === '}') {
+                    mapCount--;
+                    expectKey = true;
+                    // console.log('dec', {mapCount, char, pos})
+                }
                 switch (char) {
                     // unquoted strings contain everything up to the next line!
                     case '{':
@@ -179,19 +186,21 @@ function process(context:Context, input:string):Context {
                     case ']':
                     case ':':
                     case ',':
-                    // case '\n':
-                        // console.log('but what', char, buffer, mode);
-                        output.push([
-                            parseValue(buffer.trimEnd()),
-                            markPosition,
-                            line
-                        ]);
+                        // console.log('but what', `(${char})`, `(${buffer})`, {mode, mapCount});
+                        if ( char !== ':' && buffer.length > 0) {
+                            
+                            output.push([
+                                parseValue(buffer.trimEnd()),
+                                markPosition,
+                                line
+                            ]);
+                        }
                         mode = unset(mode, MODE_MULTI_QUOTE | MODE_VALUE);
                         // maybeWithinQuote = false;
                         buffer = '';
 
-                        if ( char !== ',') {
-                            output.push([char,pos,line]);
+                        if (char !== ',') {
+                            output.push([char, pos, line]);
                         }
                         break;
                 }
@@ -202,7 +211,7 @@ function process(context:Context, input:string):Context {
             if (char == "'" && charBuffer[1] == "'" && charBuffer[2] == "'") {
                 // mode = MODE_IDLE;
                 mode = unset(mode, MODE_MULTI_QUOTE);
-
+                
                 output.push([
                     trimMultiQuote(buffer, offset),
                     markPosition,
@@ -210,69 +219,93 @@ function process(context:Context, input:string):Context {
                 ]);
                 buffer = '';
             }
-        } else if( isSet(mode, MODE_VALUE) ){
-            if( char === ' ' || isNewline || char === ',' || char === ':' || char === ']' || char === '}' ){
-                // console.log('end value', mode, pos, char);
-                // mode = set(mode, MODE_MAYBE_VALUE);
+        } else if (isSet(mode, MODE_VALUE)) {
+            let ended = false;
+            if( endChar.length > 0 ){
+                ended = endChar.indexOf(char) !== -1;
+                if( ended ){
+                    buffer = buffer + char;
+                }
+            }
+            else 
+            {
+                ended = char === ' ' || isNewline || char === ',' || char === ':' || char === ']' || char === '}';
+            }
+            if (ended) {
+                // console.log('end value', {mode, pos, char, endChar, mapCount}, buffer);
+
                 let value = parseValue(buffer.trimEnd());
-                output.push([value,markPosition,line]);
+                // console.log('end val', {char, value, pos}, value );
+                output.push([value, markPosition, line]);
                 mode = unset(mode, MODE_VALUE);
-                // console.log('end val', char, value, pos);
-                if( char === ']' || char === '}' ){
+                expectKey = true;
+                if (char === ']' || char === '}') {
                     // output.push([ char,markPosition,line]);
-                    // buffer = '';
-                    cpos -=1;
-                    pos -=1;
-                    linePosition -=1;
+                    cpos -= 1;
+                    pos -= 1;
+                    linePosition -= 1;
                 }
                 buffer = '';
+                endChar = '';
             }
-        
-        } else if ( isSet(mode, MODE_QUOTE) ) {
+
+        } else if (isSet(mode, MODE_QUOTE)) {
             // console.log('mode quote', pos, char, isNewline);
-            if ( endChar.indexOf(char) !== -1 ) {
-                output.push([buffer,markPosition,line]);
+            if (endChar.indexOf(char) !== -1) {
+                output.push([buffer, markPosition, line]);
                 mode = unset(mode, MODE_QUOTE);
                 buffer = '';
+                endChar = '';
+                expectKey = true;
             }
-        } else if ( isSet(mode, MODE_MULTI_COMMENT) ) {
+        } else if (isSet(mode, MODE_MULTI_COMMENT)) {
             if (char == '/' && charBuffer[1] == '*') {
                 // withinMultiComment = false;
                 mode = unset(mode, MODE_MULTI_COMMENT);
             }
-        } else if ( isSet(mode, MODE_COMMENT)) {
-            if ( isNewline ) {
+        } else if (isSet(mode, MODE_COMMENT)) {
+            if (isNewline) {
                 mode = unset(mode, MODE_COMMENT);
                 // withinComment = false;
             }
         } else {
             // console.log('hmm', mode, char);
+            if (char === '{') {
+                mapCount++;
+                expectKey = true;
+                // console.log('inc', {mapCount, char, pos})
+            } else if (char === '}') {
+                mapCount--;
+                expectKey = true;
+                // console.log('dec', {mapCount, char, pos})
+            }
             switch (char) {
                 case '{':
                 case '}':
                 case '[':
                 case ']':
-                    output.push([char,pos,line]);
-                case ':':
+                    output.push([char, pos, line]);
+                    break;
+                // case ':':
                 case ',':
                     break;
                 case ' ':
                 case '\n':
                     break;
                 case '#':
-                    mode = set(mode,MODE_COMMENT);
+                    mode = set(mode, MODE_COMMENT);
                     // withinComment = true;
                     break;
                 case '"':
-                
-                    mode = set(mode,MODE_QUOTE);
+                    mode = set(mode, MODE_QUOTE);
                     // withinQuote = true;
+                    // console.log('start quote', {char,pos});
                     markPosition = pos;
                     char = '';
                     endChar = '"';
                     break;
                 case '~':
-                    mode = set(mode,MODE_QUOTE);
+                    mode = set(mode, MODE_QUOTE);
                     markPosition = pos;
                     char = '~';
                     endChar = ' \n';
@@ -290,81 +323,44 @@ function process(context:Context, input:string):Context {
         }
 
         if (
-            isSet(mode, MODE_QUOTE) 
-            || isSet(mode, MODE_VALUE) 
+            isSet(mode, MODE_QUOTE)
+            || isSet(mode, MODE_VALUE)
             // || isSet(mode, MODE_MAYBE_VALUE) 
-            || isSet(mode, MODE_MAYBE_QUOTE) 
+            || isSet(mode, MODE_MAYBE_QUOTE)
             || isSet(mode, MODE_MULTI_QUOTE)
         ) {
             buffer = buffer + char;
         }
-
-        // Log(
-        //     'readAhead',
-        //     context.position + pos,
-        //     linePosition,
-        //     char == '\n' ? '\\n' : char,
-        //     char.charCodeAt(0),
-        //     '-*',
-        //     charBuffer,
-        //     context.markPosition,
-        //     modeToString(context),
-        //     buffer
-        // );
-
-        // console.log('>>', char, buffer, mode );
     }
 
 
-    return {...context, 
+    return {
+        ...context,
         pos, buffer, endChar,
-        offset, markPosition, mode, charBuffer, 
-        output, line, linePosition };
+        mapCount, expectKey,
+        offset, markPosition, mode, charBuffer,
+        output, line, linePosition
+    };
 }
 
 
-function trimMultiQuote(buffer:string, headerOffset:number) {
+function trimMultiQuote(buffer: string, headerOffset: number) {
     // trim all whitespace up to the first character
-    buffer = trimLeft(buffer.substring(0, buffer.length - 2));
+    buffer = buffer.substring(0, buffer.length - 2).trimStart();
     let len,
         ii,
         lines = buffer.split('\n');
 
     for (ii = lines.length - 1; ii >= 0; ii--) {
-        // Log('trimMultiQuote', lines[ii]);
         lines[ii] = trimLeftMax(lines[ii], headerOffset - 2);
     }
 
     buffer = lines.join('\n');
 
-    // Log('multiquote', `>${buffer}<`);
-    // trimRight(buffer.substring(0,buffer.length-2), '\n'),
     return buffer;
 }
 
-function modeToString(mode:number) {
-    if ( isSet(mode, MODE_COMMENT) ) {
-        return 'comment';
-    }
-    if (isSet(mode, MODE_MULTI_COMMENT)) {
-        return 'multiComment';
-    }
-    if ( isSet(mode, MODE_VALUE) ) {
-        return 'value';
-    }
-    if ( isSet(mode, MODE_QUOTE) ) {
-        return 'quote';
-    }
-    if ( isSet(mode, MODE_MAYBE_QUOTE) ) {
-        return 'quote?';
-    }
-    if ( isSet(mode, MODE_MULTI_QUOTE) ) {
-        return 'multiQuote';
-    }
-    return 'idle';
-}
-
-function createContext():Context {
+function createContext(): Context {
     return {
         buffer: '',
         pos: 0,
@@ -376,16 +372,10 @@ function createContext():Context {
         linePosition: 0,
         markPosition: 0,
         charBuffer: ['', '', ''],
-        endChar:''
+        endChar: '',
+        mapCount: 0,
+        expectKey: true,
     };
-}
-
-function trimLeft(str:string) {
-    str = str.replace(/^\s\s*/, '');
-    let ws = /\s/,
-        ii = str.length;
-    while (ws.test(str.charAt(--ii)));
-    return str.slice(0, ii + 1);
 }
 
 /**
@@ -393,34 +383,14 @@ function trimLeft(str:string) {
  * @param {*} str
  * @param {*} offset
  */
-function trimLeftMax(str:string, offset:number) {
+function trimLeftMax(str: string, offset: number) {
     let ws = /\s/,
         ii = 0;
     while (ii < offset && ws.test(str.charAt(ii++)));
     return str.substring(ii - 1);
 }
 
-function trimRight(str:string, ch:string = ' ') {
-    let ii;
-    for (ii = str.length - 1; ii >= 0; ii--) {
-        if (ch != str.charAt(ii)) {
-            str = str.substring(0, ii + 1);
-            break;
-        }
-    }
-    return str;
-}
 
-/**
- *
- * @param {*} str
- */
-function parseValue(str:string) {
-    // I'm not proud...
-    try {
-        return JSON.parse(str);
-    } catch (err) {
-        // Log('parseNumber', err.message, str );
-    }
-    return str;
+function parseValue(str: string) {
+    return parseJSON(str, str);
 }
--- test/query/tokenizer.ts
+++ test/query/tokenizer.ts
@@ -9,7 +9,7 @@ const Log = createLog('TestQueryTokenizer');
 
 let test = suite('tokenizer');
 
-test('parses', async () => {
+test.skip('parses', async () => {
     let data, out;
     // out = tokenize(`[ "/component/title", "text" ] 29 :nope !d +\n`);
     out = tokenize(undefined, `[ "/component/title", "te`);
@@ -61,8 +61,7 @@ test('parses', async () => {
     Log.debug(out);
 });
 
-test('parses various', () => {
-
+test('tokenizeString', () => {
     let cases = [
         ['[29]',
             [
@@ -116,12 +115,151 @@ test('parses various', () => {
             [
                 ['file:///test/fixtures/rootA/static/', 0, 0]
             ]],
+        [`{
+            md:
+                '''
+                First line.
+                Second line.
+                  This line is indented by two spaces.
+                '''
+            }`,
+            [
+                ['{', 0, 0],
+                ['md', 14, 1],
+                [
+                    'First line.\nSecond line.\n  This line is indented by two spaces.\n',
+                    34,
+                    6
+                ],
+                ['}', 182, 7]
+            ]],
+        [
+            `{
+                # TL;DR
+                human:   Hjson
+                machine: JSON
+            }`,
+            [
+                ['{', 0, 0],
+                ['human', 42, 2],
+                ['Hjson', 51, 2],
+                ['machine', 73, 3],
+                ['JSON', 82, 3],
+                ['}', 99, 4]
+            ]
+        ],
+        [
+            `{ 'name':'finn' }`,
+            [
+                ["{", 0, 0],
+                ["name", 3, 0],
+                ["finn", 10, 0],
+                ["}", 16, 0],
+            ]
+        ],
+        [
+            `[ "oh cheese" '' size! ]`,
+            [
+                ["[", 0, 0],
+                ["oh cheese", 2, 0],
+                ["", 14, 0],
+                ["size!", 17, 0],
+                ["]", 23, 0],
+            ]
+        ],
+        [
+            `:atom { msg : :world}`,
+            [
+                [':atom', 0, 0],
+                ['{', 6, 0],
+                ['msg', 8, 0],
+                [':world', 14, 0],
+                ['}', 20, 0]
+            ]
+        ],
+        [
+            `{"@e":1004}`,
+            [ 
+                [ '{', 0, 0 ], 
+                [ '@e', 1, 0 ], 
+                [ 1004, 6, 0 ], 
+                [ '}', 10, 0 ] 
+            ]
+        ],
+        [
+            `[ "/component/dir", { "@e":1004, "uri":"file:///test/fixtures/rootA/purgatory" } ]`,
+            [
+                [ '[', 0, 0 ],
+                [ '/component/dir', 2, 0 ],
+                [ '{', 20, 0 ],
+                [ '@e', 22, 0 ],
+                [ 1004, 27, 0 ],
+                [ 'uri', 33, 0 ],
+                [ 'file:///test/fixtures/rootA/purgatory', 39, 0 ],
+                [ '}', 79, 0 ],
+                [ ']', 81, 0 ]
+            ],
+            
+        ],
+        [
+            `{ "meta":{ "author":"av" }, }`,
+            [
+                [ '{', 0, 0 ],
+                [ 'meta', 2, 0 ],
+                [ '{', 9, 0 ],
+                [ 'author', 11, 0 ],
+                [ 'av', 20, 0 ],
+                [ '}', 25, 0 ],
+                [ '}', 28, 0 ]
+            ]
+        ],
+        [
+            `[ "/component/meta", { 
+                "meta":{ 
+                    "author":"av",
+                    "tags": [ "first", "action" ] 
+                    }
+                    "createdAt":"2020-05-23T09:00:00.000Z"
+                } 
+                ]`,
+            [
+                [ '[', 0, 0 ],
+                [ '/component/meta', 2, 0 ],
+                [ '{', 21, 0 ],
+                [ 'meta', 40, 1 ],
+                [ '{', 47, 1 ],
+                [ 'author', 70, 2 ],
+                [ 'av', 79, 2 ],
+                [ 'tags', 105, 3 ],
+                [ '[', 113, 3 ],
+                [ 'first', 115, 3 ],
+                [ 'action', 124, 3 ],
+                [ ']', 133, 3 ],
+                [ '}', 156, 4 ],
+                [ 'createdAt', 178, 5 ],
+                [ '2020-05-23T09:00:00.000Z', 190, 5 ],
+                [ '}', 233, 6 ],
+                [ ']', 252, 7 ]
+            ]
+        ],
+        [
+            `{@e: 3, text: hello}`,
+            [
+                [ '{', 0, 0 ],
+                [ '@e', 1, 0 ],
+                [ 3, 5, 0 ],
+                [ 'text', 8, 0 ],
+                [ 'hello', 14, 0 ],
+                [ '}', 19, 0 ]
+            ]
+        ]
     ];
 
     cases.forEach(([input, expected]) => {
         let output = tokenizeString(input as string);
-        // Log.debug(output);
+        // console.log(input, output);
         assert.equal(output, expected as []);
+
     });
 
 });
